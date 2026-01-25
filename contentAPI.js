ed954f282ed542fbf1fe1b3b6371d798a452c5d2
shurmeri <sakari.j.hurmerinta@stundent.jyu.fi>
2026-01-25 15:08:41 +0100

    added scaffold code to contentAPI

--- contentAPI.js
+++ contentAPI.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = express.Router()
-
+ 
 
 router.get('/content', (req, res) => {
-  res.send('content!')
+  res.send('test content')
+
+  //TODO: code that fetches user's quiz if logged in
+})
+
+// If user adds question to a quiz then it is posted to database
+router.post('/content', (req, res) => {
+  res.json({userid: 123, question: '1+1x0?'})
+  res.send('added question to quiz')
 })
 
 module.exports = router
